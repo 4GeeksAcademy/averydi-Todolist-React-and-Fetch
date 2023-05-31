6a663b0dfdaea2d58113473eeb49dc4685b03375
David Avery <avery.di@gmail.com>
2023-05-31 22:28:55 +0000

    Rework and final edits for revision

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [formValue, setFormValue] = useState({});
+  
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/averydi")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setTodos(data);
+      });
+  },[]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/averydi", {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: { "Content-type": "application/json" },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log(err);
+      });
+  },[todos]);
+  
+  const addTodo = () => {
+    setTodos([...todos, formValue]);
 };
+    const inputChange = (e) => {
+      setFormValue({ label: e.target.value, done: false });
+    };
 
-export default Home;
+    const deleteTodo = (index) => {
+      setTodos((todo) => {
+        return todo.filter((item, i) => i !== index);
+      });
+    };
+    return (
+      <div>
+        <div className="form-container">
+          <input onChange={inputChange} type="text" />
+          <button onClick={addTodo}>Add Todo Item</button>
+        </div>
+        <h2>Todo List</h2>
+        {todos.length
+          ? todos.map((item, index) => {
+              return (
+                <h6>
+                  {item.label}
+                  <button onClick={() => deleteTodo(index)}>X</button>
+                </h6>
+              );
+            })
+          : null}
+      </div>
+    );
+};
+export default Todo;
